326fae1ea18ef28ce4417f3abe314bc84e30fb6f
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2019-02-09 20:09:02 +0530

    Updates

---
+++ precache-manifest.a675e80d0df3c0675a0c97fa58c4dc8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "15e18c78eb78a1dce817",
+    "url": "/react-todolist/static/js/runtime~main.15e18c78.js"
+  },
+  {
+    "revision": "1c7a56ed5c99f902ffea",
+    "url": "/react-todolist/static/js/main.1c7a56ed.chunk.js"
+  },
+  {
+    "revision": "bc5b8f5bdedf95012c8d",
+    "url": "/react-todolist/static/js/1.bc5b8f5b.chunk.js"
+  },
+  {
+    "revision": "1c7a56ed5c99f902ffea",
+    "url": "/react-todolist/static/css/main.0ebd1c9a.chunk.css"
+  },
+  {
+    "revision": "56e1c3c0ac80b6a81978719e1f8850d8",
+    "url": "/react-todolist/index.html"
+  }
+];
